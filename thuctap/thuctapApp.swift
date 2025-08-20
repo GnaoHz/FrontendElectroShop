c2e4eb74258809b17e80ff93b722aeedf1f53430
Huang <154526787+GnaoHz@users.noreply.github.com>
2025-08-20 09:45:15 +0700

    Initial Commit

---
+++ thuctap/thuctapApp.swift
@@ -0,0 +1,17 @@
+//
+//  thuctapApp.swift
+//  thuctap
+//
+//  Created by NGUYEN VIET HOANG on 20/8/25.
+//
+
+import SwiftUI
+
+@main
+struct thuctapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
